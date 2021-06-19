9df7a854a967894a5f471d7f6ad09e58b1ecc56d
Marcin Smoła <Marcinure1@gmail.com>
2021-06-19 23:15:11 +0200

    Fixup

--- components/__tests__/timer.test.js
+++ components/__tests__/timer.test.js
@@ -1,7 +1,3 @@
 describe("timer", () => {
-<<<<<<< HEAD
   it.todo("begins with timer set at 5 minutes");
-=======
-  it("begins with timer set at 5 minutes");
->>>>>>> e0faae4fcf4cc036d51303f18872bb86df4a5cd7
 });
